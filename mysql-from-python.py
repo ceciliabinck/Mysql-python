82a42a96e65c63889930db462c7bc27c5e9bf5de
ceciliabinck <cecilia4binck@gmail.com>
2020-06-30 19:12:27 +0000

    Create a table lesson

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,9 +1,11 @@
 import os
 import pymysql
+import datetime 
 
 
 
-# Get username from another environment
+# Get username from  workspace
+# modify this variable if running on 
 username = os.getenv("USER")
 
 # connect the to database
@@ -14,11 +16,12 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+
+        # note that the above will still display a warning (not errror) if the 
+        # tabel already exists
 finally:
     # close the connection, regardless of whether the above was successful
     connection.close()
