b2d5efb65db6eadb5be9980056e614133c12f7df
ceciliabinck <cecilia4binck@gmail.com>
2020-06-30 19:32:30 +0000

    Execute Many lesson

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -20,7 +20,8 @@ try:
         rows = [("Bob", 21, "1990-02-06 23:04:56"),
                 ("Jim", 56, "1955-05-09 13:12:45"),
                 ("Fred", 100, "1911-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES(%s,%s,%s);", rows)
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
         connection.commit()
 finally:
     # close the connection, regardless of whether the above was successful
+    connection.close()
